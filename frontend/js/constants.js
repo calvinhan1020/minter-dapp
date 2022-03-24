3e0f2096aa7e5e426cb101de3d696243188d33af
Calvin Han <calvinhan1020@gmail.com>
2022-03-25 07:54:11 +0800

    FRONTEND UPDATED

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x34119366b150ebe1cAd0Af0e3ee3CbA578342A75"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the ScoGang NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The ScoGang NFT Project is a community driven project. We want to make our NFT accessible to most human on earth at an affordable price point of 0.065 ETH, and 0.045 ETH for presale.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
